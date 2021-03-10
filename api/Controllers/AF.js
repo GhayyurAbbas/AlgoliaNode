13b84c33d2c8794247d3d921fafd4f1e1b2e3ccc
azuma <43908571+kid420@users.noreply.github.com>
2021-03-10 16:56:56 +0500

    3_10_2020 before leaving

--- api/Controllers/AF.js
+++ api/Controllers/AF.js
@@ -12,6 +12,7 @@ fs = require('fs');
 var assert=require('assert')
 const BBVConstants=require('../../BBVConstants')
 const isNullOrWhiteSpace=require('../../isNullOrWhiteSpace')
+const BBVItem = require('../Models/BBVItem')
 
 // async function  get_temp(){
 //     let d=[]
@@ -40,26 +41,96 @@ console.log('entering the parsed function')
    
     var oo=[];
     var a=obj.ReturnVoucherObj.Vouchers[0].Voucher;
+    var lastitem=null;
+    console.log('starting looop');
+
     for(i=0;i<a.length;i++)
     {
-        oo.push({
-            BBVID:BBVConstants.BBV_AF_ID_Prefix+a[i].VoucherID[0],
-            PromotionID: a[i].VoucherID[0],
-            Advertiser: a[i].MerchantSiteName[0],
-            AdvertiserID: a[i].ProgrammeID[0],
-            DataType: (assert(a[i].VoucherCode[0],'N/A')||isNullOrWhiteSpace(a[i].VoucherCode[0]))?'2':'1',
-            Code:a[i].VoucherCode[0],
-            Title:a[i].VoucherDescription[0],
-            ShortDescription:a[i].CategoryName[0],
-            Categories:a[i].CategoryName[0],
-            Starts:a[i].StartDate[0],
-            Ends:a[i].EndDate[0],
-            DeepLinkTracking:a[i].Tracking_URL[0],
-            LogoLink:a[i].ImageURL[0],
-            objectID:BBVConstants.BBV_AF_ID_Prefix+a[i].VoucherID[0],
-     
-        })
+        var additem=true;
+        var item=new BBVItem();
+           item.BBVID=BBVConstants.BBV_AF_ID_Prefix+a[i].VoucherID[0];
+           item.PromotionID= a[i].VoucherID[0];
+           item.Advertiser= a[i].MerchantSiteName[0];
+           item.AdvertiserID= a[i].ProgrammeID[0];
+           item.DataType= (assert(a[i].VoucherCode[0],'N/A')||isNullOrWhiteSpace(a[i].VoucherCode[0]))?'2':'1';
+           item.Code=a[i].VoucherCode[0];
+           item.Title=a[i].VoucherDescription[0];
+           item.ShortDescription=a[i].CategoryName[0];
+           item.Categories=a[i].CategoryName[0];
+           item.Starts=a[i].StartDate[0];
+           item.Ends=a[i].EndDate[0];
+           item.DeepLinkTracking=a[i].Tracking_URL[0];
+           item.LogoLink=a[i].ImageURL[0];
+           item.objectID=BBVConstants.BBV_AF_ID_Prefix+a[i].VoucherID[0];
+           //fp
+    console.log('first point');
+    console.log(item); 
+         if(isNullOrWhiteSpace(item.DeepLinkTracking)){
+            continue
+        }
+if(lastitem!=null)
+{
+    if(assert(lastitem.BBVID,item.BBVID) && assert(lastitem.PromotionID,item.PromotionID) && assert(lastitem.Code,item.Code))
+    {
+        additem=false
+        
+        if(!lastitem.Categories.includes(item.Categories))
+        {
+            lastitem.Categories += ',' + item.Categories;
+        }
+    } 
+}
+//sp
+console.log('second point')
+if(additem)
+{
+    if(isNullOrWhiteSpace(item.Starts))
+    {
+        continue;
     }
+    var start=new Date(item.Starts).toUTCString();
+    var end;
+    if(!isNullOrWhiteSpace(item.Ends))
+    {
+        end=new Date(item.Ends);
+    }
+    else
+    {
+        end=new Date(start.getDate()+5);
+        item.SaveLocalKey=false;
+    }
+    //tp
+    console.log('third point')
+    var now=Date.now();
+    item.Starts=String(start+":dd/MM/yyyy HH:mm:ss");
+    item.Ends=String(end+':dd/MM/yyyy HH:mm:ss')
+    console.log(item.Starts)
+    item.StartUnixTimeStamp = Math.floor(new Date(start).getTime()/1000);
+    item.EndUnixTimeStamp = Math.floor(new Date(end).getTime()/1000);
+
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if(Math.ceil(Math.abs(start - Date.now()) / (1000 * 60 * 60 * 24))>=1)
+    {
+        item.Status="Upcoming";
+        oo.push(item);
+        lastitem=item;
+    }
+    else if(Math.ceil(Math.abs(end - Date.now()) / (1000 * 60 * 60 * 24))>=1)
+    {
+        item.Status="Active";
+        oo.push(item);
+        lastitem=item;
+    }
+    else
+    {
+        item.Status="Expired";
+    }
+    console.log('item added')
+
+}
+    }
+    console.log('done')
   //  console.log(oo)
     return oo;
 }
---
+++ api/Models/BBVItem.js
@@ -0,0 +1,46 @@
+
+class BBVItem {
+    constructor() {
+       this.BBVID='';
+       this.PromotionID= '';
+       this.Advertiser='';
+       this.AdvertiserID= '';
+       this.DataType= ''
+       this.Code='';
+       this.Title='';
+       this.ShortDescription='';
+       this.Starts='';
+       this.Ends='';
+       this.DeepLinkTracking='';
+       this.LogoLink='';
+       this.objectID='';
+       this.LongDescription ='';
+       this.Terms ='';
+       this.Status ='';
+       this.Deeplink ='';
+       this.Categories ='';
+       this.Regions ='';
+       this.Exclusive ='';
+       this.DateAdded ='';
+       this.serviceArea ='';
+       this.StartUnixTimeStamp ='';
+       this.EndUnixTimeStamp ='';
+       this.DealType ='';
+       this.SaveLocalKey = true;
+       this.StoreCategoriesArray ='';
+       this.StoreDescription ='';
+       this.StoreDetailsID ='';
+       this.InternalLink ='';
+       this.Domains ='';
+       this.AdvertiserKnownNames ='';
+       this.Clicks ='';
+       this.Likes ='';
+       this.TwitterLink ='';
+       this.FbLink ='';
+       this.Comments ='';
+       this.Rating ='';
+       this.Rank ='';
+       this.serviceCountry ='';
+    }
+  }
+module.exports=BBVItem
