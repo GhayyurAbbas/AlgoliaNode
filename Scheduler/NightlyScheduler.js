21948c38d2e8fc31733a083e8d1a6f4ba99b041a
azuma <43908571+kid420@users.noreply.github.com>
2021-03-16 14:55:33 +0500

    Update NightlyScheduler.js

--- Scheduler/NightlyScheduler.js
+++ Scheduler/NightlyScheduler.js
@@ -3,7 +3,7 @@ let shell=require('shelljs');
 const af=require('../api/Controllers/AF');
 exports.nightly_schedular=function()
 {
-    cron.schedule('0 52 14 * * *',function(){
+    cron.schedule('0 0 0 * * *',function(){
         console.log('fully fueled and reloaded');
         af.script_vouchers();
         if(shell.exec("dir").code !==0)
